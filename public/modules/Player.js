0c11db9eb20d64461fdda8577691a8a3da379296
Skylar Salerno <skylar.salerno@gmail.com>
2023-03-16 08:10:06 -0400

    simplify player to data structure

--- public/modules/Player.js
+++ public/modules/Player.js
@@ -1,41 +1,8 @@
 export default class Player {
     constructor(id, soundManager) {
         this.id = id || Math.random()
-        this.soundManager = soundManager
-        this.osc = {}
         this.notes = []
         this.pressed = false
-        this.playing = false
-        this.pPlaying = false // previously Playing
-        this.gainNode = {}
-    }
-
-    playSound(instrument, canvasHeight) {
-        this.soundManager.playSound(instrument, Math.round(20 * (1 - this.curY)))
-        
-    }
-    
-    addVisualNote(x, y, isPlaying) {
-
-        this.notes.unshift({
-            x: x, y: y, solid: isPlaying, s: isPlaying ? 25 - Math.random() * 10 : 20 - Math.random() * 15
-        })
-        // don't store more than 70 notes
-        if (this.notes.length == 70) {
-            this.notes.pop()
-        }
-    }
-    updatePosition = (x, y) => {
-        this.curX = x;
-        this.curY = y;
-    }
-
-    startPlaying = (e) => {
-        this.pressed = true
-        this.shouldPlay = true
-    }
-
-    stopPlaying = (e) => {
-        this.pressed = false
-    }
+        this.souldPlay = false
+    } 
 }
--- public/modules/PlayersManager.js
+++ public/modules/PlayersManager.js
@@ -1,75 +1,85 @@
 import Player from "./Player.js"
 
-export default class PlayersManager{
+export default class PlayersManager {
 
-    constructor(soundManager){
-      this.soundManager = soundManager
-      this.user = new Player(Math.random(), soundManager)
-      this.players = [this.user]
-      this.lastSent = null // when we last sent data to the server
-      this.socket = io();
-      this.socket.on('drawing', this.recievePlayerUpdate);
-    }
-  
-    updateUser = (x, y, isPressed = null) => {
-      let user = this.user
-      // isPressed is null for mousemove, true for mouse down, false for mouseup
-      user.curX = x
-      user.curY = y
-      if (isPressed !== null) {
-        //
-        user.pressed = isPressed
-        user.shouldPlay = isPressed
-      }
-  
-      if (performance.now() - this.lastSent > 50) {
-        this.sendPlayerUpdate()
-      }
+  constructor(soundManager) {
+    this.soundManager = soundManager
+    this.user = new Player(Math.random(), soundManager)
+    this.players = [this.user]
+    this.lastSent = null // when we last sent data to the server
+    this.socket = io();
+    this.socket.on('drawing', this.recievePlayerUpdate);
+  }
+
+  updateUser = (x, y, isPressed = null) => {
+    let user = this.user
+    // isPressed is null for mousemove, true for mouse down, false for mouseup
+    user.curX = x
+    user.curY = y
+    if (isPressed !== null) {
+      //
+      user.pressed = isPressed
+      user.shouldPlay = isPressed
     }
 
-    updateNotes(frame, canvasWidth, canvasHeight){
-      this.players.forEach((player) => {
-        const willPlay = frame == 0 && (player.shouldPlay || player.pressed)
-        /* animation rate is 4 times the rate of musical notes
-            so notes only play on frame 0 out of 4
-            this is better than separate loops because separate loops may not start at exactly the same time
-          */
-        if (willPlay) {
-          const instrument = player == this.user ? "chorus" : "piano"
-          player.playSound(instrument)
-        }
-
-    const deNormalizedX = player.curX * canvasWidth
-    const deNormalizedY = player.curY * canvasHeight
-        player.addVisualNote(deNormalizedX, deNormalizedY, player.pressed || player.shouldPlay)
-      })
-    }
-  
-    sendPlayerUpdate = () => {
-      this.socket.emit('drawing', {
-        id: this.user.id,
-        curX: this.user.curX,
-        curY: this.user.curY,
-        pressed: this.user.pressed,
-        shouldPlay: this.user.shouldPlay
-      });
-      this.lastSent = performance.now();
-    }
-  
-    recievePlayerUpdate = (msg) => {
-      let player = this.players.find(player => player.id == msg.id)
-      if (!player) {
-        player = new Player(parseFloat(msg.id), this.soundManager);
-        this.players.push(player)
-      }
-      Object.assign(player,
-        {
-          curX: msg.curX,
-          curY: msg.curY,
-          pressed: msg.pressed,
-          shouldPlay: msg.shouldPlay
-        }
-      )
+    if (performance.now() - this.lastSent > 50) {
+      this.sendPlayerUpdate()
     }
   }
-  
+
+  updateNotes(frame, canvasWidth, canvasHeight) {
+    this.players.forEach((player) => {
+      const willPlay = frame == 0 && (player.shouldPlay || player.pressed)
+      /* animation rate is 4 times the rate of musical notes
+          so notes only play on frame 0 out of 4
+          this is better than separate loops because separate loops may not start at exactly the same time
+        */
+      if (willPlay) {
+        const instrument = player == this.user ? "chorus" : "piano"
+        this.soundManager.playSound(instrument, Math.round(20 * (1 - player.curY)))
+      }
+
+      const deNormalizedX = player.curX * canvasWidth
+      const deNormalizedY = player.curY * canvasHeight
+      this.addVisualNote(player, deNormalizedX, deNormalizedY, player.pressed || player.shouldPlay)
+    })
+  }
+
+  sendPlayerUpdate = () => {
+    this.socket.emit('drawing', {
+      id: this.user.id,
+      curX: this.user.curX,
+      curY: this.user.curY,
+      pressed: this.user.pressed,
+      shouldPlay: this.user.shouldPlay
+    });
+    this.lastSent = performance.now();
+  }
+
+  addVisualNote(player, x, y, isPlaying) {
+
+    player.notes.unshift({
+      x: x, y: y, solid: isPlaying, s: isPlaying ? 25 - Math.random() * 10 : 20 - Math.random() * 15
+    })
+    // don't store more than 70 notes
+    if (player.notes.length == 70) {
+      player.notes.pop()
+    }
+  }
+
+  recievePlayerUpdate = (msg) => {
+    let player = this.players.find(player => player.id == msg.id)
+    if (!player) {
+      player = new Player(parseFloat(msg.id), this.soundManager);
+      this.players.push(player)
+    }
+    Object.assign(player,
+      {
+        curX: msg.curX,
+        curY: msg.curY,
+        pressed: msg.pressed,
+        shouldPlay: msg.shouldPlay
+      }
+    )
+  }
+}
